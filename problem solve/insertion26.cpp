4f8c05956ea7053abda166b6b9b5b01249c15b48
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-10-13 01:27:52 +0600

    some file add

--- problem solve/insertion26.cpp
+++ problem solve/insertion26.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int arr[]={4,7,10,5,8};
 int n=5;
+int x=-1;
+
 int main(){
-    for
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            for(int j=n-1;j>i;j--){
+                arr[j+1]=arr[j];
+            }
+            arr[i+1]=-1;
+            n++;
+            i++;
+        }
+    }
+   
+
+    cout<<"After array insert:";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
