b7b6ab988523a14e4102cd7a43c313e5e14c9823
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-02-04 01:39:49 +0600

    Largest && smalest value search

---
+++ problem solve/problem_number1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3]={23,56,21};
+    int Largest_value=arr[0];
+    int Smalest_value=arr[0];
+
+    for(int i=0;i<3;i++){
+        if(arr[i]>Largest_value){;
+         Largest_value=arr[i];
+         cout<<"Largest value:"<<arr[i]<<endl;
+       
+    }
+    else if(arr[i]<Smalest_value){
+        Smalest_value=arr[i];
+        cout<<"Smalest Value:"<<arr[i];
+        
+    }
+}
+}
