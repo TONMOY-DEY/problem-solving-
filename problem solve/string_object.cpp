3eb4c9372a6edeaa9f063f5ecbca68e23ba92224
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-02-24 21:56:29 +0600

    string object

---
+++ problem solve/string_object.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"please!Enter the number:"<<endl;
+    getline(cin,name);
+
+    cout<<"Hello, "<<name<<endl;
+}
