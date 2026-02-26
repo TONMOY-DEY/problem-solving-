8a2e4eeb3869dd127a7e63b87d2831c42ceecf26
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-02-27 02:46:40 +0600

    add

---
+++ problem solve/standard.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size[8]={55,72,83,61,90,45,78,67};
+        
+        for(int i=0;i<8;i++){
+            cout<<size[i]<<" ";
+        }
+    }
