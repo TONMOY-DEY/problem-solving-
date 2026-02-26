f0bfe7d2440397143158af3441317973d495acf7
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-02-27 02:53:03 +0600

    solve problem

--- problem solve/standard.cpp
+++ problem solve/standard.cpp
@@ -4,6 +4,9 @@ int main(){
     int size[8]={55,72,83,61,90,45,78,67};
         
         for(int i=0;i<8;i++){
-            cout<<size[i]<<" ";
+            cout<<size[i];
+        if(i<7){
+            cout<<" ,";
+        }
         }
     }
