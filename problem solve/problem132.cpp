bf87dd5b7170507647cad96ee96b3cf039ac1d22
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-03-02 00:48:25 +0600

    selection sort

--- problem solve/problem132.cpp
+++ problem solve/problem132.cpp
@@ -1,5 +1,33 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[10]={}
+    int n;
+    
+    cout<<"please!Enter the number:"<<endl;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cout<<"please ! Enter the index number:";
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        int minINDEX=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minINDEX]){
+                minINDEX=j;
+            }
+
+        }
+        int temp=arr[i];
+        arr[i]=arr[minINDEX];
+        arr[minINDEX]=temp;
+
+    }
+    cout<<"Sorted array:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+}
 }
