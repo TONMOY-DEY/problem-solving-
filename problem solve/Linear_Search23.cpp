4d2bd9d75c4b4086a31ac832829eecf219bd97cd
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-03-16 22:19:58 +0600

    add 1st linear search

---
+++ problem solve/Linear_Search23.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5]={1,2,3,4,5};
+    int x;
+
+    cin>>x;
+    for(int i=0;i<5;i++){
+        if(a[i]==x){
+            cout<<"Found";
+
+        }
+        
+
+    }
+}
