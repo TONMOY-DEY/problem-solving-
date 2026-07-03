cc43b406fa7a292d107c543df2313db55fa2bedd
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-07-03 19:19:24 +0600

    problem statement c

---
+++ problem solve/problem_set(3)/problem1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    int y;
+    cin>>x>>y;
+
+    cout<<x <<"+" <<y <<"="<<x+y <<endl;
+    cout<<x <<"*"<<y <<"="<<x*y <<endl;
+    cout<<x <<"-"<<y <<"="<<x-y <<endl;
+
+}
