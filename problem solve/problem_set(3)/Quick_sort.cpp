49553242cc7267f90b1fe68cd09eed06cd9c61d1
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-07-07 14:36:50 +0600

    Update Quick_sort.cpp

--- problem solve/problem_set(3)/Quick_sort.cpp
+++ problem solve/problem_set(3)/Quick_sort.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 using namespace std;
 
-
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
+  
 int partition(int arr[], int low, int high) {
     int pivot = arr[high]; 
     int i = (low - 1);  
@@ -19,7 +19,6 @@ int partition(int arr[], int low, int high) {
             swap(&arr[i], &arr[j]);
         }
     }
-
     swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
@@ -27,19 +26,15 @@ int partition(int arr[], int low, int high) {
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-
-        
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
-
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
-
 int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
